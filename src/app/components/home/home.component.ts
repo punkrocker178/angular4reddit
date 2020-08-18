c3af7da0a46ad8e3526c8298eace488e6e6a4828
Steve Le <steve.le@contemi.com>
2020-08-18 21:17:06 +0700

    Done implementing load more data

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RedditListingService } from 'src/app/services/reddit-listing.service';
 import { Observable, from, merge, forkJoin, BehaviorSubject, Subscription, Subject, zip } from 'rxjs';
 import { ApiList } from 'src/app/constants/api-list';
@@ -11,12 +11,13 @@ import { Post } from 'src/app/model/post';
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   listings: Observable<Listings>;
-  posts: Post[];
+  posts: Post[] = [];
   posts$: Observable<Post[]>;
   listingsSubject: BehaviorSubject<Listings> = new BehaviorSubject<Listings>(null);
+
   after: string;
   isLoading: boolean = true;
 
@@ -26,23 +27,42 @@ export class HomeComponent implements OnInit {
   constructor(private redditService: RedditListingService, private authenService: RedditAuthenticateService) { }
 
   ngOnInit(): void {
-    this.redditService.getListigs(ApiList.LISTINGS_HOT, { limit: 30 }).pipe(
-      tap(next => {
-        this.after = next.after;
-        this.listingsSubject.next(next);
-      }
-      )).subscribe(_ => this.isLoading = false);
-
-    this.posts$ = this.listingsSubject.pipe(map(res => this.posts = [... this.posts, ...res.children]));
+    this.fetchData();
+    this.posts$ = this.listingsSubject.pipe(
+      map(res => {
+        this.posts = [...this.posts, ...res.children];
+        return this.posts;
+      })
+    );
 
   }
 
   loadMore() {
-    this.redditService.getListigs(ApiList.LISTINGS_HOT, { limit: 30, after: this.after }).pipe(
+    this.fetchData(this.after);
+  }
+
+  fetchData(after?: string) {
+    let queryParams = {
+      limit: 30
+    }
+
+    if (after) {
+      queryParams['after'] = this.after
+    }
+
+    this.redditService.getListigs(ApiList.LISTINGS_HOT, queryParams).pipe(
       tap(next => {
         this.after = next.after;
         this.listingsSubject.next(next);
-      })).subscribe();
+      },
+        takeUntil(this.listingsSubject)
+      )).subscribe(_ => this.isLoading = false);
+
+  }
+
+  ngOnDestroy() {
+    this.listingsSubject.next(null);
+    this.listingsSubject.unsubscribe();
   }
 
 
