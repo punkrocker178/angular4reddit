05f5c3f886c0ed5d17c72dcb0d7efeb446ca1782
Steve Le <steve.le@contemi.com.vn>
2020-09-04 11:05:46 +0000

    Support embeded video

--- src/app/components/post/components/list-posts-item/post.component.ts
+++ src/app/components/post/components/list-posts-item/post.component.ts
@@ -13,10 +13,22 @@ export class PostComponent {
   }
 
   isVideo() {
-    return this.post.data['is_video'];
+    return this.post.data['is_video'] || this.post.data['media'];
   }
 
   getVideo() {
+
+    //  Get embeded link from iframe element returned
+    if (!this.post.data['is_video']) {
+      const iframeHtml = this.post.data['media']['oembed']['html'];
+      const srcAttr = iframeHtml.match(/src="(.*?)"/g)[0];
+      const srcValue = srcAttr.match(/"(.*?)"/g)[0].replace(/amp;/g, '');
+      
+      //  Remove double quotes
+      const url = srcValue.substring(1, srcValue.length -1);
+      return decodeURI(url);
+    }
+
     return this.post.data['media']['reddit_video']['fallback_url'];
   }
 
