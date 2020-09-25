e4e73b51f32a7d556e5ee8c2b840bb64b43825a1
Steve Le <steve.le@contemi.com.vn>
2020-09-25 11:14:28 +0000

    Done implementation of recursive comments There are issues, and need to implement load more comments

--- src/app/components/post/components/post-comments/post-comments.component.ts
+++ src/app/components/post/components/post-comments/post-comments.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Post } from 'src/app/model/post';
 import { RedditListingService } from 'src/app/services/reddit-listing.service';
 import { tap } from 'rxjs/operators';
 
 @Component({
     selector: 'post-comments',
-    templateUrl: './post-comments.html'
+    templateUrl: './post-comments.html',
+    // changeDetection: ChangeDetectionStrategy.OnPush
   })
 export class PostCommentsComponent {
 
   @Input() comments;
+  @Input() isReplies;
+
   moreComments;
   
   constructor(
@@ -17,7 +20,19 @@ export class PostCommentsComponent {
   ) {}
   
   ngOnInit() {
+    if (typeof this.comments === 'object') {
+      if (this.comments.data && this.comments.data['children']) {
+        this.comments = this.comments.data['children'];  
+      }
+    }
+
     this.moreComments = this.comments.pop();
-    console.log(this.moreComments);
+  }
+
+  /* Issue: ExpressionChangedAfterItHasBeenCheckedError */
+  hasReplies(comment) {
+    return comment.data['replies'] && 
+    comment.data['replies']['data']['children'] && 
+    comment.data['replies']['data']['children'].length > 0;
   }
 }
