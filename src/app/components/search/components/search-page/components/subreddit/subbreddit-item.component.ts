3b6bbd6f7068e968a0522ba9d2e2c05fe0d2256d
punkrocker178 <ngochieu.le1708@gmail.com>
2021-04-09 10:51:55 +0700

    Implement subscribe subreddit API

--- src/app/components/search/components/search-page/components/subreddit/subbreddit-item.component.ts
+++ src/app/components/search/components/search-page/components/subreddit/subbreddit-item.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 import { CheckDeviceFeatureService } from 'src/app/services/check-device-feature.service';
+import { SubredditService } from 'src/app/services/subreddit.service';
  
 @Component({
     selector: 'subreddit-item',
@@ -11,11 +14,22 @@ export class SubbredditItemComponent {
 
     icon: string;
     @Input() subredditData;
+    subscribe$ = new Subject();
 
-    constructor(private router: Router, private checkDeviceFeatureService: CheckDeviceFeatureService) {}
+    constructor(
+        private router: Router,
+        private checkDeviceFeatureService: CheckDeviceFeatureService,
+        private subredditService: SubredditService) {}
 
     ngOnInit() {
         this.icon = this.subredditData.data.community_icon || this.subredditData.data.icon_img;
+        this.subscribe$.pipe(
+            switchMap((action:string) => this.subredditService.subscribeSubreddit(action, this.subredditData.data.name)),
+            tap(next => {
+                console.log(next);
+                this.subredditData.data.user_is_subscriber = !this.subredditData.data.user_is_subscriber; 
+            })
+        ).subscribe();
     }
 
     isUserSubscriber() {
@@ -28,6 +42,16 @@ export class SubbredditItemComponent {
 
     isMobile() {
         return this.checkDeviceFeatureService.isMobile();
+    }
+
+    subscribe() {
+        const action = this.isUserSubscriber() ? 'unsub' : 'sub';
+        this.subscribe$.next(action);
+    }
+
+    ngOnDestroy() {
+        this.subscribe$.complete();
+        this.subscribe$.unsubscribe();
     }
   
 }
