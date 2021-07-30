da59f9b332fcee4582cdb44693937ff00b2b7c61
hieule178 <ngochieu.le1708@gmail.com>
2021-07-30 13:35:18 +0700

    Added environment files, history of these 2 files are rewritten

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    functionUrl: 'https://446ov6467l.execute-api.ap-southeast-1.amazonaws.com/prod',
+    loginUrl: 'https://446ov6467l.execute-api.ap-southeast-1.amazonaws.com/prod/gw/authorize',
+    appURL: 'https://angular4reddit.me/'
+  };
