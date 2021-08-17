7b6513017aa25d09c255490f28e13b9931a90211
punkrocker178 <ngochieu.le1708@gmail.com>
2021-08-17 21:31:30 +0700

    Enhance authenticate component to handle user decline oauth2 permission

--- src/app/components/authenticate/authenticate.component.ts
+++ src/app/components/authenticate/authenticate.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ActivatedRoute, Router } from '@angular/router';
 import { RedditAuthenticateService } from 'src/app/services/reddit-authenticate.service';
 import { LocalStorageService } from 'src/app/services/localStorage.service';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { of, Subject, throwError } from 'rxjs';
+import { catchError, mergeMap, takeUntil } from 'rxjs/operators';
 import { UserService } from "src/app/services/user.service";
 import { PreferencesService } from "src/app/services/preferences.service";
 import { UserInterface } from "src/app/model/user.interface";
@@ -15,10 +15,12 @@ import { UserInterface } from "src/app/model/user.interface";
 
 export class AuthenticateComponent implements OnInit, OnDestroy {
     private ngUnsubscribe = new Subject();
-    data = {};
+
+    isError: boolean;
+    errorMsg: string;
 
     constructor(
-        private activatedRoute: ActivatedRoute, 
+        private activatedRoute: ActivatedRoute,
         private authenService: RedditAuthenticateService,
         private localStorage: LocalStorageService,
         private userService: UserService,
@@ -26,25 +28,45 @@ export class AuthenticateComponent implements OnInit, OnDestroy {
         private router: Router) { }
 
     ngOnInit() {
-        this.activatedRoute.queryParams.subscribe(params => {
-            if (params['code'] && params['state'] === this.localStorage.get('state')) {
-                this.data['code'] = params['code'];
-                this.authenService.getBearerAPI(params['code']).subscribe(res => {
 
-                    if (res['token_type'] === 'bearer') {
-                        this.authenService.getUserInfo()
-                        .pipe(takeUntil(this.ngUnsubscribe))
-                        .subscribe((res: UserInterface) => {
-                            this.authenService.storeUserDetail(res);
-                            this.userService.setUser(res);
-                            this.preferenceService.setPreference('safeBrowsing', !res.over_18)
-                            this.router.navigateByUrl('/home');
-                        });
-                    }
-
-                });
+        const getUserInfo = (res: any) => {
+            if (res['token_type'] === 'bearer') {
+                return this.authenService.getUserInfo()
             }
-        });
+
+            if (res['error']) {
+                throw throwError(res['error']);
+            }
+        };
+
+        const getAccessToken = (params: any) => {
+            if (params['code'] && params['state'] === this.localStorage.get('state')) {
+                return this.authenService.getBearerAPI(params['code']).pipe(
+                    mergeMap(getUserInfo)
+                )
+            }
+
+            if (params['error']) {
+                throw throwError(params['error']);
+            }
+        };
+
+        this.activatedRoute.queryParams.pipe(
+            takeUntil(this.ngUnsubscribe),
+            mergeMap(getAccessToken),
+            catchError(error => {
+                return error;
+            })
+        ).subscribe(
+            (data: UserInterface) => {
+                this.authenService.storeUserDetail(data);
+                this.userService.setUser(data);
+                this.preferenceService.setPreference('safeBrowsing', !data.over_18)
+                this.router.navigateByUrl('/home');
+            }, err => {
+                this.errorMsg = err;
+            }
+        );
     }
 
     ngOnDestroy() {
