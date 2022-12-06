3804c4ff2a8b3576ee8331aa684ab5e382de7863
punkrocker178 <ngochieu.le1708@gmail.com>
2022-12-07 00:25:47 +0700

    Update prod app url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     functionUrl: 'https://446ov6467l.execute-api.ap-southeast-1.amazonaws.com/prod',
     loginUrl: 'https://446ov6467l.execute-api.ap-southeast-1.amazonaws.com/prod/gw/authorize',
-    appURL: 'https://angular4reddit.me/'
+    appURL: 'https://dbqo2nayu6ql6.cloudfront.net/'
   };
